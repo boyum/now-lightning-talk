d69a669c452e67bc00cd7b06527785c18ca22cf1
Restyled.io <commits@restyled.io>
2020-04-30 08:02:09 +0000

    Restyled by autopep8

--- api/python.py
+++ api/python.py
@@ -1,17 +1,18 @@
 from http.server import BaseHTTPRequestHandler
 from datetime import datetime
 
+
 class handler(BaseHTTPRequestHandler):
 
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'application/json')
-    self.end_headers()
-    self.wfile.write(
-      """
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(
+            """
       {
         "time": "{time}",
         "lang": "python"
       }
       """.format(time=str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode()))
-    return
+        return
