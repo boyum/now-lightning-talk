1369abacc1a0a33e1a012b1d2cf5a1b1b07b9409
Sindre Bøyum <sindre.boyum@gmail.com>
2020-04-30 09:12:45 +0200

    oops

--- api/is-it-danbo-day.go
+++ api/is-it-danbo-day.go
@@ -7,14 +7,13 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	danboDayIII := time.Date(2020, 4, 30, 0, 0, 0, 0, time.UTC)
 	currentTime := time.Now()
 	year, month, day := currentTime.Date()
 
-	if year == 2020 && month == "April" && day == 30 {
-		fmt.Fprintf(w, "<div style=\"display:flex;align-items:center;justify-content:center;min-height:100vh;font-size:50px;text-align:center;\">YES</div>")
-		
-	}
-	else {
+	if currentTime == danboDayIII { 
+		fmt.Fprintf(w, "<div style=\"display:flex;align-items:center;justify-content:center;min-height:100vh;font-size:50px;text-align:center;\">YES</div>")		
+	} else {
 		fmt.Fprintf(w, "<div style=\"display:flex;align-items:center;justify-content:center;min-height:100vh;font-size:50px;text-align:center;\">nooooo</div>")
 	}
 } 
