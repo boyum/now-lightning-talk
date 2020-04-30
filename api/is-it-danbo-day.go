1222dd0fa3e1800d546f850aa824685ceeee4c3d
Sindre Bøyum <sindre.boyum@gmail.com>
2020-04-30 09:16:47 +0200

    oopsie

--- api/is-it-danbo-day.go
+++ api/is-it-danbo-day.go
@@ -9,7 +9,6 @@ import (
 func Handler(w http.ResponseWriter, r *http.Request) {
 	danboDayIII := time.Date(2020, 4, 30, 0, 0, 0, 0, time.UTC)
 	currentTime := time.Now()
-	year, month, day := currentTime.Date()
 
 	if currentTime == danboDayIII { 
 		fmt.Fprintf(w, "<div style=\"display:flex;align-items:center;justify-content:center;min-height:100vh;font-size:50px;text-align:center;\">YES</div>")		
